c80d4e05b41596a931f2e7645fc659f2f7cd5426
Alex Sayers <alex@asayers.com>
2021-07-13 20:27:44 +0900

    Move drawing code to Display impl

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,10 @@
-#[derive(Debug, PartialEq, Clone, Copy)]
+use chrono::{Datelike, Month, NaiveDate, Utc, Weekday};
+use enum_map::Enum;
+use num_traits::FromPrimitive;
+use std::fmt::{self, Display};
+use yansi::{Color, Paint};
+
+#[derive(Debug, PartialEq, Clone, Copy, Enum)]
 pub enum Season {
     Winter,
     Lent,
@@ -25,6 +31,14 @@ impl Season {
         }
     }
 
+    pub fn weeks(self) -> std::ops::Range<u8> {
+        if self == Season::Advent {
+            self.starting_week()..52
+        } else {
+            self.starting_week()..self.succ().starting_week()
+        }
+    }
+
     pub fn succ(self) -> Season {
         use Season::*;
         match self {
@@ -37,5 +51,63 @@ impl Season {
             Autumn => Advent,
             Advent => Winter,
         }
+    }
+}
+
+impl Display for Season {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let season_name = format!("{:?}", self);
+        writeln!(f, "   ┌───────┤{:^8}├──────┐", season_name)?;
+        writeln!(f, "   │ Mo Tu We Th Fr  Sa Su │")?;
+        writeln!(f, "───┼───────────────────────┤")?;
+        let today = Utc::today();
+        let year = today.year();
+        for week in self.weeks() {
+            let mut new_month = None;
+            write!(f, "w{} │", week - self.starting_week() + 1)?;
+            for &day in &[
+                Weekday::Mon,
+                Weekday::Tue,
+                Weekday::Wed,
+                Weekday::Thu,
+                Weekday::Fri,
+                Weekday::Sat,
+                Weekday::Sun,
+            ] {
+                let date = if week == 0 {
+                    NaiveDate::from_isoywd(year - 1, 53, day)
+                } else {
+                    NaiveDate::from_isoywd(year, week.into(), day)
+                };
+                let color = if date.month() % 2 == 0 {
+                    Color::Blue
+                } else {
+                    Color::Magenta
+                };
+                if date.day() == 1 {
+                    new_month = Some(date.month());
+                }
+                if day == Weekday::Sat {
+                    write!(f, " ")?;
+                }
+                if date == today.naive_local() {
+                    write!(f, " {}", Paint::new(format!("{:2}", date.day())).bold())?;
+                } else {
+                    write!(f, " {}", color.paint(format!("{:2}", date.day())))?;
+                }
+            }
+            write!(f, " │")?;
+            if let Some(m) = new_month {
+                let color = if m % 2 == 0 {
+                    Color::Blue
+                } else {
+                    Color::Magenta
+                };
+                write!(f, "  {:?}", color.paint(Month::from_u32(m).unwrap()))?;
+            }
+            writeln!(f)?;
+        }
+        f.write_str("───┴───────────────────────┘\n")?;
+        Ok(())
     }
 }
