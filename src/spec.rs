e170a9fa6d3e4cdbe2ba796d31ac339e6f566f4c
Alex Sayers <alex@asayers.com>
2022-06-28 20:36:24 +0900

    Add DateObject

--- src/spec.rs
+++ src/spec.rs
@@ -1,6 +1,45 @@
+use crate::eight::{Season, YearSeason};
 use anyhow::{anyhow, ensure};
-use chrono::{Datelike, IsoWeek, NaiveDate, Utc, Weekday};
+use chrono::{Datelike, IsoWeek, Month, NaiveDate, Utc, Weekday};
 use std::str::FromStr;
+
+enum DateObject {
+    Year(i32),
+    Month(i32, Month),
+    Week(i32, u8),
+    Day(NaiveDate),
+    Season(YearSeason),
+}
+
+impl FromStr for DateObject {
+    type Err = anyhow::Error;
+    fn from_str(txt: &str) -> anyhow::Result<DateObject> {
+        let mut tokens: Vec<&str> = txt.split('-').collect();
+        if tokens.is_empty() {
+            return Err(anyhow!("Empty string"));
+        }
+        let year = match tokens[0].parse() {
+            Ok(x) if tokens[0].len() == 4 => {
+                tokens.remove(0);
+                x
+            }
+            _ => Utc::now().year(),
+        };
+        Ok(if tokens.is_empty() {
+            DateObject::Year(year)
+        } else if let Ok(month) = tokens[0].parse::<Month>() {
+            DateObject::Month(year, month)
+        } else if let Ok(season) = tokens[0].parse::<Season>() {
+            DateObject::Season(YearSeason { year, season })
+        } else if let Some(week) = tokens[0].strip_prefix('w') {
+            DateObject::Week(year, week.parse().unwrap())
+        } else {
+            let month = tokens[0].parse()?;
+            let day = tokens[1].parse()?;
+            DateObject::Day(NaiveDate::from_ymd(year, month, day))
+        })
+    }
+}
 
 fn new_week(year: i32, week: u32) -> anyhow::Result<IsoWeek> {
     Ok(NaiveDate::from_isoywd_opt(year, week, Weekday::Wed)
